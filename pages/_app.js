fbf5ecf40b65a7b54286a1ff0b58dd99d40ca025
Your Name <you@example.com>
2020-11-02 09:09:42 +0800

    add

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../static/react-datetime.css';
+import '../static/index.css';
+import '../static/autosuggest.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
